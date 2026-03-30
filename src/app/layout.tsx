64658340ebcbe3cd650e341c70ee664411828001
vincentism <hkustliwenqian@hotmail.com>
2026-03-30 15:02:11 +0800

    chore: remove next/font Google Fonts dependency, use system font stack

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Sanic + EdgeOne Pages",
@@ -28,7 +17,7 @@ export default function RootLayout({
         <link rel="icon" href="/sanic-favicon.svg" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="antialiased"
       >
         {children}
       </body>
